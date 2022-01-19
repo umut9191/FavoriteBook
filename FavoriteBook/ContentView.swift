9f14088e8f197596a5c2b44a7cddf133e1b98d73
umut9191 <umutsurmeli91@gmail.com>
2022-01-19 22:33:52 +0300

    Initial Commit

---
+++ FavoriteBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FavoriteBook
+//
+//  Created by Mac on 19.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
